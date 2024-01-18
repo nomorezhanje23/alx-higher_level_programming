a6329206e7e6f2e6e697a3067cbc2f30649e37f3
nomorezhanje23 <nomorezhanje23@gmail.com>
2024-01-18 12:40:31 -0800

    code fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from add_0 import add
 
-    x = 1
-    y = 2
+    a = 1
+    b = 2
 
-    print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
