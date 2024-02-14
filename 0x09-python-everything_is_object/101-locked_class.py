3ac76106e73685e59a27a9bdfb9fd578035bf99a
nomorezhanje23 <nomorezhanje23@gmail.com>
2024-02-13 16:58:06 -0800

    0x09-python-everything_is_object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Has a class, LockedClass.
+"""
+
+
+class LockedClass:
+    """
+
+    Defines LockedClass that  allows only to create specific
+    class or object attributes.
+
+    """
+    __slots__ = ["first_name"]
